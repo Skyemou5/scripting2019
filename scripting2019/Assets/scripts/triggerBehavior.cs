7db24b4ebc337084ee0c7287d479703b0e0cbccd
Ben Shurts <benjaminshurts@gmail.com>
2019-08-21 14:30:35 -0600

    more stuff

---
+++ scripting2019/Assets/scripts/triggerBehavior.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerBehavior : MonoBehaviour
+{
+    public FloatData data;
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
