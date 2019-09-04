fef78014148051559a4768346b9fd8571817a8f2
Ben Shurts <benjaminshurts@gmail.com>
2019-09-04 14:22:52 -0600

    event system

---
+++ scripting2019/Assets/scripts/ImageController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ImageController : MonoBehaviour
+{
+    private Image imgComp;
+    public UnityEvent updateImageEvent;
+    private void Start()
+    {
+        imgComp = GetComponent<Image>();
+    }
+    
+    public void UpdateImageComponent(float amt)
+    {
+        imgComp.fillAmount += amt;
+    }
+
+    public void UpdateImageComponent(FloatData dataObj)
+    {
+        imgComp.fillAmount = dataObj.value;
+    }
+
+    private void Update()
+    {
+        updateImageEvent.Invoke();
+    }
+}
